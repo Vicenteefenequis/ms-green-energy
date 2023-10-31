682cb979ac8b8da49786e1868017371e37650b7f
Vicente Nascimento <vicente_nascimento@sicredi.com.br>
2023-10-31 13:18:31 -0300

    feat: adding more than cov

--- core_apps/projects/admin.py
+++ core_apps/projects/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DataEnergetic, Location, Project
+
+
+class ProjectsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Project, ProjectsAdmin)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Location, LocationAdmin)
+
+
+class DataEnergeticAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(DataEnergetic, DataEnergeticAdmin)
